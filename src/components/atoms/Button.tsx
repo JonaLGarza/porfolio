33142ddc9150c5a9a7634a184c802e0d722ed553
JonaLGarza <jonathanlgarza@outlook.com>
2025-04-13 16:31:34 -0600

    added more props to custom button

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -2,9 +2,15 @@ import { Button as ShadButton } from "../ui/button";
 
 interface ButtonProps {
   children: React.ReactNode;
-  variant?: "default" | "outline";
+  variant?: "default" | "outline" | "secondary" | "ghost" | "destructive" | "link";
+  asChild?: boolean;
+  size?: "default" | "sm" | "lg" | "icon";
 }
 
-export default function Button({ children, variant = "default" }: ButtonProps) {
-  return <ShadButton variant={variant}>{children}</ShadButton>;
+export default function Button({ children, variant = "default", asChild = false, size = "default" }: ButtonProps) {
+  return (
+    <ShadButton variant={variant} asChild={asChild} size={size}>
+      {children}
+    </ShadButton>
+  );
 }
