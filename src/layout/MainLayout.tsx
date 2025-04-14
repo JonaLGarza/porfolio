eef8a73750043b10385d6d482d12484c567a3243
JonaLGarza <jonathanlgarza@outlook.com>
2025-04-14 17:35:08 -0600

    added dark mode toggle to main Layour

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,6 +1,11 @@
 import { Outlet, NavLink } from "react-router-dom";
+import { useTheme } from "next-themes";
+import Button from "../components/atoms/Button";
+import { Moon, Sun } from "lucide-react";
+import React from "react";
 
 export default function MainLayout() {
+  const { theme, setTheme } = useTheme();
   return (
     <div className="min-h-screen p-6">
       <header className="flex justify-between items-center mb-10">
@@ -10,6 +15,17 @@ export default function MainLayout() {
           <NavLink to="/projects" className={({ isActive }) => isActive ? "text-blue-600 font-semibold" : "text-gray-600"}>Projects</NavLink>
           <NavLink to="/contact" className={({ isActive }) => isActive ? "text-blue-600 font-semibold" : "text-gray-600"}>Contact</NavLink>
         </nav>
+        <div>
+          Toggle {theme === "dark" ? "light" : "dark"} mode
+          <Button
+            variant="ghost"
+            size="icon"
+            onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+            aria-label="Toggle Theme"
+          >
+            {theme === "dark" ? <Sun className="size-4" /> : <Moon className="size-4" />}
+          </Button>
+        </div>
       </header>
       <Outlet />
     </div>
