7719953a74f5265d4c617c3e6e3805a9adb41af3
JonaLGarza <jonathanlgarza@outlook.com>
2025-04-17 14:36:25 -0600

    added base for gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+const isProd = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), tsconfigPaths({
-    projects: ["tsconfig.app.json"],
-  }),],
-})
+  base: isProd ? '/porfolio/' : '/',
+  plugins: [
+    react(),
+    tailwindcss(),
+    tsconfigPaths({
+      projects: ["tsconfig.app.json"],
+    }),
+  ],
+});
