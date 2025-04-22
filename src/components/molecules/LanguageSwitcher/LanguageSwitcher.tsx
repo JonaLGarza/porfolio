a3e4a24bdcf7c5078e9cf15fa3bc366b193f5c36
JonaLGarza <jonathanlgarza@outlook.com>
2025-04-22 01:42:21 -0600

    added styles and basic component implementation to LanguagueSwitcher

--- src/components/molecules/LanguageSwitcher/LanguageSwitcher.tsx
+++ src/components/molecules/LanguageSwitcher/LanguageSwitcher.tsx
@@ -1,14 +1,15 @@
 import i18n from 'i18next';
 import { useTranslation } from 'react-i18next';
+import Button from '../../atoms/Button';
 
 export function LanguageSwitcher() {
   const changeLanguage = (lng: string) => i18n.changeLanguage(lng);
   const { t } = useTranslation();
   return (
     <div className="space-x-2 flex">
-      <p>{t('language')}</p>
-      <button onClick={() => changeLanguage('en')}>🇺🇸 EN</button>
-      <button onClick={() => changeLanguage('es')}>🇲🇽 ES</button>
+      <p className='self-center mb-1'>{t('language')}</p>
+      <Button className='cursor-pointer' onClick={() => changeLanguage('en')}>🇺🇸 EN</Button>
+      <Button className='cursor-pointer' onClick={() => changeLanguage('es')}>🇲🇽 ES</Button>
     </div>
   );
 }
